dd94663f43f5ac5430af7275c9cfd0011aba4577
Alejandro Mata <alemalvarez@icloud.com>
2024-04-16 09:46:36 -0700

    General cleanup, comments and best practices

--- utils.py
+++ utils.py
@@ -2,24 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from torch.utils.data import DataLoader
-from torch import tensor
+import torch
 
 def tensor_info(tensor, name):
-    print(f"Type of {name}: {type(tensor)}")
+    print(f"==================== {name} tensor info ====================")
+    print(f"Type: {type(tensor)}")
     # For np arrays:
     if(isinstance(tensor, np.ndarray)):
-        print(f"Shape of {name}: {tensor.shape}")
-        if tensor.ndim == 1:
-            print(f"{name}: {tensor}")
+        print(f"Shape: {tensor.shape}")
+        if tensor.ndim == 1 or tensor.shape[0] == 1:
+            print(f"Content: {tensor}")
         elif tensor.ndim == 2:
             print(f"{name}[0]: {tensor[0]}")
     # For DataLoaders:
     if(isinstance(tensor, DataLoader)):
         print(f"Batch size: {tensor.batch_size}")
         for X, y in tensor:
-            print(f"Shape of features in first batch of {name}: {X.shape}")
-            print(f"Shape of labels in first batch of {name}: {y.shape} {y.dtype}")
+            print(f"Shape of features in first batch: {X.shape}")
+            print(f"Shape of labels in first batch: {y.shape} {y.dtype}")
             break
+    # For torch tensors:
+    if(isinstance(tensor, torch.Tensor)):
+        print(f"Shape: {tensor.shape}")
+        if tensor.ndim == 1 or tensor.shape[0] == 1:
+            print(f"Content: {tensor}")
     print("")
 
 def draw_image(img, label=None):
@@ -38,3 +44,8 @@ def draw_grid(images, labels=None):
         plt.axis("off")
         plt.imshow(images[i].squeeze(), cmap="gray")
     plt.show()
+
+def batch_accuracy(y_hat, y):
+    predicted = torch.argmax(y_hat, 1)
+    correct = (predicted == y).sum().item()
+    return correct / y.size(0)
